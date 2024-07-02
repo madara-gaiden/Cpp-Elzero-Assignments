d023d45798e0f3e375303408299a6da138cf49e4
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-02 22:49:44 +0100

    Update

--- W003_Data Types/A002.cpp
+++ W003_Data Types/A002.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     double salary = 5000.98;
-    cout << "The bytes of " << salary << " are : " << sizeof(salary); 
-// 8 Bytes
-// 64 Bits
+    cout << "The bytes of " << salary << " are : " << sizeof(salary) << " Bytes" << endl; 
+    cout << "The Bits of " << salary << " are : " << sizeof(salary) * 8 << " Bits" << endl;
+    return 0;
 }
