153168c430780ce224bdfb8a438b3a393e41aec6
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-03 00:08:01 +0100

    Update 5 & 6

--- W003_Data Types/A005.cpp
+++ W003_Data Types/A005.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main(){
     char a = '~', b = '&', c = '%', d = 'A';
     
-    cout << "ASCII Value of ~ is " << int(a) << endl; // 126
-    cout << "ASCII Value of & is " << int(b) << endl; // 38
-    cout << "ASCII Value of % is " << int(c) << endl; // 37
-    cout << "ASCII Value of A is " << int(d) << endl; // 65
+    cout << "ASCII Value of " << a << " is " << int(a) << endl; // 126
+    cout << "ASCII Value of " << b << " is " << int(b) << endl; // 38
+    cout << "ASCII Value of " << c << " is " << int(c) << endl; // 37
+    cout << "ASCII Value of " << d << " is " << int (d) << endl; // 65
     return 0;
 }
