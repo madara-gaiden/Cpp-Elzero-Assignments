8854dc962b1da0cefbdcbdf240edc62a0adec882
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-03 01:15:30 +0100

    Update 9

---
+++ W003_Data Types/A009.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Edit Anything Except Values
+    unsigned a = 100;
+    signed b = -100;
+    short c = 100;
+    long double d = 500.55;
+
+    // Do Not Edit
+    cout << a << "\n"; // 100
+    cout << b << "\n"; // -100
+    cout << c << "\n"; // 100
+    cout << d << "\n"; // 500.55
+}
