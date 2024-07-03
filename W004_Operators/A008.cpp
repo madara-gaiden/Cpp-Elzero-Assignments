b2f32feb88e6a5309a96aa4162b260d75991b5c8
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-04 00:10:02 +0100

    Update W4A8

--- W004_Operators/A008.cpp
+++ W004_Operators/A008.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int num = 10;   
+    int num = 10;       
 
     cout << --num << " | " << num++ << " | " << ++num << "\n";
     cout << num++ << " | " << num << " | " << num-- << "\n";
