32be5db5a16428cb34401164e8d3be7c69472016
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-04 20:04:26 +0100

    W5P1A3

---
+++ W005P1_Control_Flow_Conditions/A003.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Please Type A Number Between 0 And 150\n";
+    cin >> num;
+
+    if (num < 10)
+        cout << "00" << num;
+    else if(num >= 10 && num < 100)
+        cout << "0" << num;
+    else
+        cout << num;
+}
