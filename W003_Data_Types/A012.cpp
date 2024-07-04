566785fe1a31472a31fb048524314f647bc59688
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-04 16:27:07 +0100

    Update W3A12

--- W003_Data_Types/A012.cpp
+++ W003_Data_Types/A012.cpp
@@ -1,14 +1,15 @@
-        #include <iostream>
-        using namespace std;
+#include <iostream>
+using namespace std;
 
-        int main(){
-            short a = 1000;
-            int b = 10000;
-            long double c = 5.560000505012;
+int main(){
+    short a = 1000;
+    int b = 10000;
+    long double c = 5.560000505012;
 
-            cout << b - (b - a) << "\n"; // 12
-            // cout << ??? + ??? << "\n"; // 20
-            cout << c * c << "\n"; // 32
-            cout << a * int(c) << "\n"; // 5000
-            // cout << char(??? * ???) << "\n"; // P = 80
-        }   
+    cout << int(c + (c + a/a)) << "\n"; // 12
+    cout << (int(c) + int(c)) + (int(c) + int(c)) << "\n"; // 20
+    cout << int(int(c) * (c + a/a)) << "\n"; // 32
+    cout << a * int(c) << "\n"; // 5000
+    cout << char(b/a * (int(c) + a/a + a/a + a/a)) << "\n"; // P = 80   
+    return 0;
+}   
