3037788f2c96f983d2244e05f0bf0b62266ca002
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-04 19:42:15 +0100

    W5P1A1

---
+++ W005P1_Control_Flow_Conditions/A001.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 40;
+    int points = 800;
+    float rate = 8.5f;
+
+    if (age > 18 && points > 500 && rate > 5)
+        cout << "Yes Age > 18 & Points > 500 & Rate > 5";
+    
+}
