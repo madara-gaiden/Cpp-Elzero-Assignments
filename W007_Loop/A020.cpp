dc0dde87c7f0602715d16f66cc7fb4cbc71c4da1
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-07 12:22:07 +0100

    Update W7A20

---
+++ W007_Loop/A020.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string names[] = {"Osama", "Ahmed", "Mahmoud", "Hagar", "Eman", "Salwa"};
+    for (int i = 0; i < int(size(names)); i++)
+    {
+        if (size(names[i]) == 5)
+        {
+            cout << names[i] << "\n";
+        }
+    }
+    return 0;
+}
