b8041759146297450547fc46e62d0e3810ec1219
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-07 04:29:08 +0100

    Update W7A14

---
+++ W007_Loop/A014.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 2;
+    while (num < 520)
+    {
+        if (num == 2)
+        {
+            num = 1;
+            cout << num << "\n";
+        }
+        else
+        {
+            cout << num << "\n";
+        }
+
+        num = num * 2 + 2;
+    }
+    return 0;
+}
