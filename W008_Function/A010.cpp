0bcab987d8732013cc4c16c171473e82bcbbd6c9
Madara Gaiden <madara.gaiden@protonmail.com>
2024-07-27 16:43:18 +0100

    W8A10

---
+++ W008_Function/A010.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int minpositive(int numbers[], int numssize) {
+    int i = 0 , minN;
+    for(;i < numssize;i++) {
+        if(numbers[i] > 0){
+            if (minN < numbers[i]) {
+                minN = minN;
+            }
+            else if(minN > numbers[i]) {
+                minN = numbers[i];
+            }
+        }
+    }
+    return minN;
+}
+
+int main()
+{
+  int numbers[] = { -10, -20, 15, 100, 10, 5, -50, 0 }; // 5
+  int numssize = size(numbers);
+  cout << minpositive(numbers, numssize) << "\n";
+  return 0;
+}
